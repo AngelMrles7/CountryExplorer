12871f2fac97403342d624db3f24d51e65271691
Angel Morales Chocue <angelmrles7@gmail.com>
2024-03-29 12:35:28 -0500

    fist commit

---
+++ src/app/shared/pages/homePage/homePage.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-home-page',
+  template: `<p>homePage works!</p>`,
+  styles: [],
+})
+export class HomePageComponent {}
